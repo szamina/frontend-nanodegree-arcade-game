a57842a3d6b46bbb3434139f91716ee5eb477297
Szabina Rezeli <szabinarezeli@gmail.com>
2018-06-04 19:37:26 +0200

    feat: define allEnemies, Player and start location of Player

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+let Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-
+    // set up x, y, coordinates and speed
     this.x = x;
     this.y = y;
     this.speed = speed;
@@ -26,20 +26,27 @@ Enemy.prototype.render = function() {
 };
 
 // Now write your own player class
+let Player = function(x, y) {
+  this.x = x;
+  this.y = y;
+}
 // This class requires an update(), render() and
 // a handleInput() method.
 
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
+// all enemy objects in an array called allEnemies
+let allEnemies = [];
 // Place the player object in a variable called player
 
+// start location of player
+let player = new Player(202, 0);
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
