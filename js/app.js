fb4e0cb9f2a9cc2446efce23d582571bb68a7934
Szabina Rezeli <szabinarezeli@gmail.com>
2018-06-04 20:16:37 +0200

    feat: define location and speed of bugs

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + (this.speed * dt);
 };
 
 // Draw the enemy on the screen, required method for game
@@ -45,6 +46,11 @@ Player.prototype.render = function() {
 // Now instantiate your objects.
 // all enemy objects in an array called allEnemies
 let allEnemies = [];
+allEnemies.push(new Enemy(0,61,600));
+allEnemies.push(new Enemy(0,145,200));
+allEnemies.push(new Enemy(0,225,100));
+allEnemies.push(new Enemy(0,225,200));
+
 // Place the player object in a variable called player
 
 // start location of player
